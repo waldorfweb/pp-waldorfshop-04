4bab412d7fac5204009cc4e2638e49af16599b02
waldorfweb <54469302+waldorfweb@users.noreply.github.com>
2022-03-02 14:48:21 +0100

    Initial commit

---
+++ src/Widgets/Common/SpacerWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Waldorfshop3\Widgets\Common;
+
+use Ceres\Widgets\Helper\BaseWidget;
+
+class SpacerWidget extends BaseWidget
+{
+    protected $template = "Waldorfshop3::Widgets.Common.SpacerWidget";
+}
---
+++ src/Widgets/Header/HeaderWidget.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Waldorfshop3\Widgets\Header;
+
+use Ceres\Widgets\Helper\BaseWidget;
+
+class HeaderWidget extends BaseWidget
+{
+
+    protected $template = "Waldorfshop3::Widgets.Header.HeaderWidget";
+}
