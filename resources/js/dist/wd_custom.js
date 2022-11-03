a4fcb6cda2b11c8d5e82560b0b9a38ba2afbe280
waldorfweb <alex@waldorfshop.eu>
2022-11-03 09:40:37 +0100

    wd_custom.js clean

--- resources/js/dist/wd_custom.js
+++ resources/js/dist/wd_custom.js
@@ -99,16 +99,16 @@ $(window).on("load", function() {
     // Popupstore Hack
 
 
-    $('.popupstore a').attr('href', '#');
+    //$('.popupstore a').attr('href', '#');
     //$('.popupstore .thumb-content a').attr('href','#');
     //$('.popupstore .invisible a').attr('href','#');
-    $('.popupstore .cmp-product-thumb').css('overflow', 'visible');
+    //$('.popupstore .cmp-product-thumb').css('overflow', 'visible');
     //$('.popupstore .invisible li').css('overflow','visible');
     //$('.popupstore .d-lg-block i').replaceWith('<i class="fa fa-lg mobile-icon-right fas fa-star " style="color:#b92a38; position: absolute; right: -16px; font-size: 48px; top: -4px; z-index:10"></i>');
     //$('.popupstore li i').replaceWith('<i class="fa fa-lg mobile-icon-right fas fa-star " style="color:#b92a38; position: absolute; right: -16px;  font-size: 36px; top: -4px; z-index:10"></i>');
     //$('.popupstore .d-lg-block i').unwrap();
 
-    $('.popupstore li ').prepend('<i class="fa fa-lg mobile-icon-right fas fa-star " style="color:#b92a38; position: absolute; right: 0px;  font-size: 36px; top: -4px; z-index:10"></i>');
+    //$('.popupstore li ').prepend('<i class="fa fa-lg mobile-icon-right fas fa-star " style="color:#b92a38; position: absolute; right: 0px;  font-size: 36px; top: -4px; z-index:10"></i>');
 
 
 
@@ -360,7 +360,7 @@ $(window).on("load", function() {
     //Login Passwort Hinweis
     //$(".modal-title, .login-view-title").append("<div class='passworthinweis'>Liebe Kunden, wie Sie vielleicht bermerkt haben ist unser neuer Webshop da. Daher müssen wir Sie leider bitten ein neues Passwort mit der Funktion \"Passwort vergessen\" zu vergeben. </div>");
 
-    $("#login .login-pwd-reset form").append("<div class='passworthinweis gastbestellung'>Hier können Sie als Gast bestellen ohne sich registrieren oder anmelden zu müssen. <br /><button href=\"/anmelden\" class=\"btn btn-primary btn-appearance btn-medium\">Weiter mit Gastbestellung. <i class=\"fa fa-user-secret\"></i></button></div>");
+    //$("#login .login-pwd-reset form").append("<div class='passworthinweis gastbestellung'>Hier können Sie als Gast bestellen ohne sich registrieren oder anmelden zu müssen. <br /><button href=\"/anmelden\" class=\"btn btn-primary btn-appearance btn-medium\">Weiter mit Gastbestellung. <i class=\"fa fa-user-secret\"></i></button></div>");
 
     //Widget Startseite
 
